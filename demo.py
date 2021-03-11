ef5b16167ba4334d8434aeb0ec9d3e55ed10cdcb
joannafg <joannafg@vt.edu>
2021-03-11 10:25:47 -0500

    read video

--- demo.py
+++ demo.py
@@ -3,12 +3,24 @@ import cv2 as cv
 print("OpenCV version:")
 print(cv.__version__)
 
-img = cv.imread('pumpkin.jpg')
-
+# read image 
+img = cv.imread('./media/pumpkin.jpg')
 # the name of the video, the image 
 cv.imshow('pumpkin', img)
 
-# wait from keyboard action? 
-cv.waitKey(0)
+# read video 
+capture = cv.VideoCapture('media/running.mp4')
+while True: 
+    isTrue, frame = capture.read()
+    # frame is the frame 
+    cv.imshow('Video', frame)
+
+    # if 'd' key is pressed, break out of the loop 
+    if cv.waitKey(20) & 0xFF==ord('d'): 
+        break 
+
+
+# cv.waitKey(0) # wait from keyboard action? 
+capture.release()
 cv.destroyAllWindows()
-exit()
+sys.exit()
